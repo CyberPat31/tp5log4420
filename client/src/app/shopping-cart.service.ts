171f3e12bb3c439f1e0bdcefea7c161b77dc862c
BastienDubourg <43591483+BastienDubourg@users.noreply.github.com>
2018-12-02 15:38:55 -0500

    Add files via upload

---
+++ client/src/app/shopping-cart.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Config } from './config';
+
+/**
+ * Defines an Item.
+ */
+export class Item {
+  productId: number;
+  quantity: number;
+}
+
+@Injectable()
+export class ShoppingCartService {
+
+  /**
+   * Handles the current error.
+   *
+   * @param error                   The error to handle.
+   * @return {Promise<object>}      A promise object.
+   */
+  private static handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.feedbackMessage || error);
+  }
+
+  /**
+   * Initializes a new instance of the ShoppingCartService class.
+   *
+   * @param http                    The HTTP service to use.
+   */
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Gets all the items in the shopping cart.
+   *
+   * @return {Promise<Item[]>}   A promise that contains the items in the shopping cart.
+   */
+  getItems(): Promise<Item[]> {
+    const url = `${Config.apiUrl}/shopping-cart`;
+
+    return this.http.get(url)
+      .toPromise()
+      .then(items => items as Item[])
+      .catch(ShoppingCartService.handleError);
+  }
+
+  /**
+   * Adds the item with the specified ID to the shopping cart.
+   *
+   * @param productId               The product ID associated with the product to add.
+   * @param quantity                The quantity of product to add.
+   * @returns {Promise<{}>}    
+   */
+  a(productId: number, quantity: number): Promise<{}> {
+    const url = `${Config.apiUrl}/products/${productId}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const options = { headers: headers, withCredentials: true };
+    
+    return this.http.post(url, JSON.stringify({
+      productId: productId,
+      quantity: quantity
+    }), options)
+    .toPromise()
+    .then()
+    .catch(ShoppingCartService.handleError);
+  }
+}
