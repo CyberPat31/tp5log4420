4ee9724ab28d87b902d16b5b12249463dc209059
BastienDubourg <43591483+BastienDubourg@users.noreply.github.com>
2018-12-03 00:07:57 -0500

    Update products.component.ts

--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Product } from '../products.service';
 
 /**
  * Defines the component responsible to manage the display of the products page.
@@ -8,5 +10,28 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html'
 })
 export class ProductsComponent {
-  // TODO: À compléter
+  
+  constructor(
+  private productService: ProductsService) { }
+
+  productCount: number;
+  products: Product[];
+  savedCriteria: string;
+  savedCategory: string;
+
+  ngOnInit() {
+  this.productCount = 0;
+  this.savedCriteria = 'price-asc';
+  this.savedCategory = 'all';
+  this.getProducts(this.savedCategory,this.savedCriteria);
+  }
+  
+  getProducts(category: string, criteria: string): void {
+    this.productService.getProducts(criteria, category)
+    .then(products => {
+      this.products = products;
+      this.savedCategory = category;
+      this.savedCriteria = criteria;
+      this.productCount = products.length;)}
+  }
 }
